64c9a607e173ef74e6e660a77847a782342e6959
Ezra Tambo <Ezratash@gmail.com>
2022-06-30 11:12:46 +0300

    This is the Final Project!

---
+++ app/src/main/java/com/example/vont_oil/facebook.kt
@@ -0,0 +1,11 @@
+package com.example.vont_oil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class facebook : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook)
+    }
+}
